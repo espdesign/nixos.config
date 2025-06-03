ac8924cd8c92c677c37197d7fd3f8fa78b824637
root <root@evansp.com>
2025-06-02 23:57:58 -0400

    added ghostty

--- home.nix
+++ home.nix
@@ -9,6 +9,8 @@
   # Packages that should be installed to the user profile.
   home.packages = with pkgs; [
     discord
+    #ghostty
+    firefox
 
     zip
     xz
@@ -24,67 +26,77 @@
     glow # markdown previewer in terminal
     btop # replacement of htop/nmon
   ];
+  programs = {
+    # basic configuration of git
+    git = {
+      enable = true;
+      userName = "Evan Pendergraft";
+      userEmail = "evanpendergraft@gmail.com";
+    };
 
-  # basic configuration of git
-  programs.git = {
-    enable = true;
-    userName = "Evan Pendergraft";
-    userEmail = "evanpendergraft@gmail.com";
-  };
-
-  programs.nvf = {
-    enable = true;
-    # your settings need to go into the settings attribute set
-    # most settings are documented in the appendix
-    # https://notashelf.github.io/nvf/options.html
-    settings = {
-      vim.vimAlias = true;
-      vim = {
-        theme = {
-          enable = true;
-          name = "gruvbox";
-          style = "dark";
-        };
-        lsp.enable = true;
-        lsp.formatOnSave = true;
-        statusline.lualine.enable = true;
-        telescope.enable = true;
-        autocomplete.nvim-cmp.enable = true;
-        clipboard.enable = true;
-        navigation.harpoon.enable = true;
-
-        languages = {
-          enableTreesitter = true;
-          nix = {
+    nvf = {
+      enable = true;
+      # your settings need to go into the settings attribute set
+      # most settings are documented in the appendix
+      # https://notashelf.github.io/nvf/options.html
+      settings = {
+        vim.vimAlias = true;
+        vim = {
+          theme = {
             enable = true;
-            format.enable = true;
+            name = "gruvbox";
+            style = "dark";
           };
-          python.enable = true;
+          lsp.enable = true;
+          lsp.formatOnSave = true;
+          statusline.lualine.enable = true;
+          telescope.enable = true;
+          autocomplete.nvim-cmp.enable = true;
+          clipboard.enable = true;
+          navigation.harpoon.enable = true;
+
+          languages = {
+            enableTreesitter = true;
+            nix = {
+              enable = true;
+              format.enable = true;
+            };
+            python.enable = true;
+          };
         };
       };
     };
-  };
 
-  # install zsh
-  programs.zsh = {
-    enable = true;
-    enableCompletion = true;
-    #    autosuggestions.enable = true;
-    #    syntaxHighlighting.enable = true;
-    shellAliases = {
-      ll = "ls -l";
-      edit = "sudo -e";
-      update = "sudo nixos-rebuild switch";
+    # install zsh
+    zsh = {
+      enable = true;
+      enableCompletion = true;
+      #    autosuggestions.enable = true;
+      #    syntaxHighlighting.enable = true;
+      shellAliases = {
+        ll = "ls -l";
+        edit = "sudo -e";
+        update = "sudo nixos-rebuild switch";
+      };
+    };
+    starship = {
+      enable = true;
+      enableZshIntegration = true;
+      settings = {
+      };
+    };
+    # Install firefox.
+    firefox.enable = true;
+
+    ghostty = {
+      enable = true;
+      settings = {
+        theme = "dark:GruvboxDark,light:GruvboxLight";
+        window-decoration = "none";
+        font-size = 14;
+      };
     };
   };
-  programs.starship = {
-    enable = true;
-    enableZshIntegration = true;
-    settings = {
-    };
-  };
-  # Install firefox.
-  programs.firefox.enable = true;
   # This value determines the home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new home Manager release introduces backwards
