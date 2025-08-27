1647847b265226163ab2d42bb6d24ef4fdfaba29
Evan Pendergraft <evanpendergraft@gmail.com>
2025-08-26 22:36:13 -0400

    added firefox to esp-sin

--- home/espdesign/esp-sin.nix
+++ home/espdesign/esp-sin.nix
@@ -1,1 +1,19 @@
-{ config, ... }: { imports = [ ./home.nix ../common ]; }
+{config, ...}: {
+  imports = [
+    ./home.nix
+    ../common
+    ../features/cli # add cli features
+    ../features/desktop # add desktop gui features
+  ];
+
+  features = {
+    cli = {
+      #these are located in /home/features/cli/*.nix
+      fish.enable = true;
+      fzf.enable = true;
+    };
+    desktop = {
+      firefox.enable = true;
+    };
+  };
+}
