5eb2edaec1437cb18b83642c6aa532f88d821219
Evan Pendergraft <evanpendergraft@gmail.com>
2025-08-27 11:35:05 -0400

    add dotfiles

--- home/espdesign/dotfiles/bat.nix
+++ home/espdesign/dotfiles/bat.nix
@@ -1,9 +1,9 @@
 #example showing how to add simple dotfiles
-# {
-#   home.file.".config/bat/config".text = ''
-#     --theme="Dracula"
+{
+  home.file.".config/bat/config".text = ''
+    --theme="Dracula"
 
-#     # Show line numbers, Git modifications and file header (but no grid)
-#     --style="numbers,changes,header"
-#   '';
-# }
+    # Show line numbers, Git modifications and file header (but no grid)
+    --style="numbers,changes,header"
+  '';
+}
