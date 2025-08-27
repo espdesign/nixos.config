2b35f36fc70bc06582f808c70b3b9777b649d8b7
Evan Pendergraft <evanpendergraft@gmail.com>
2025-08-27 11:33:20 -0400

    again example bat.nix

--- home/espdesign/dotfiles/default.nix
+++ home/espdesign/dotfiles/default.nix
@@ -1,4 +1,7 @@
 {inputs,...}:{
+    imports = [
+    ./bat.nix #currently commented out but an example on how to add simple dotfiles
+    ];
     home.file.".config/nvim" = {
     source = "${inputs.dotfiles}/nvim";
     recursive = true;
