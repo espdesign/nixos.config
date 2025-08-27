b75dac37f66c784f191f508b7aeb690cf7d38b0b
Evan Pendergraft <evanpendergraft@gmail.com>
2025-08-26 22:16:51 -0400

    change to services.puseaudio.enable

--- hosts/esp-sin/configuration.nix
+++ hosts/esp-sin/configuration.nix
@@ -74,7 +74,7 @@
     openFirewall = true;
   };
   # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
+  services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
