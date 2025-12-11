f96404166321a69ee52ff6c52f44b6259f8863c6
espdesign <evanpendergraft@gmail.com>
2025-12-10 21:11:53 -0500

    added gnome-user.nix and wallpaper

---
+++ modules/home-manager/gnome-user.nix
@@ -0,0 +1,36 @@
+{
+  config,
+  pkgs,
+  ...
+}: {
+  # 1. Install the Wallpaper file
+  home.file.".local/share/backgrounds/wallpaper-molly.jpg".source = ../../assets/wallpapers/wallpaper-molly.jpg;
+
+  # 2. Configure GNOME Settings
+  dconf = {
+    enable = true;
+    settings = {
+      "org/gnome/shell" = {
+        disable-user-extensions = false;
+        # Enable the app indicator extension (tray icons)
+        enabled-extensions = ["appindicatorsupport@rgcjonas.gmail.com"];
+        favorite-apps = [
+          "org.gnome.Nautilus.desktop"
+          "firefox.desktop"
+          "com.mitchellh.ghostty.desktop"
+        ];
+      };
+
+      "org/gnome/desktop/interface" = {
+        show-battery-percentage = true;
+        clock-format = "12h";
+      };
+
+      # Set desktop background for dark and light mode
+      "org/gnome/desktop/background" = {
+        picture-uri = "file://${config.home.homeDirectory}/.local/share/backgrounds/wallpaper-molly.jpg";
+        picture-uri-dark = "file://${config.home.homeDirectory}/.local/share/backgrounds/wallpaper-molly.jpg";
+      };
+    };
+  };
+}
