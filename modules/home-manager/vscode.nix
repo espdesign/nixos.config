5c63c1cce3e4d2a7eabdc852ee1be5ce6e869eed
espdesign <evanpendergraft@gmail.com>
2026-01-12 14:24:42 -0500

    make specific dev-server.nix file, and move zed-editor to its own module

--- modules/home-manager/vscode.nix
+++ modules/home-manager/vscode.nix
@@ -1,13 +1,10 @@
-{pkgs, ...}: {
+{ pkgs, ... }:
+{
   programs.vscode = {
     enable = true;
     extensions = with pkgs.vscode-extensions; [
       dracula-theme.theme-dracula
       vscodevim.vim
     ];
-  };
-  programs.zed-editor = {
-    enable = true;
-    extensions = ["monokai-pro-ce"];
   };
 }
