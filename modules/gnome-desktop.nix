922d6276ba4f2c5b181112220f57f360e2ddc8aa
espdesign <evanpendergraft@gmail.com>
2026-03-02 11:51:14 -0500

    added thunderbird chrome fix launcher

--- modules/gnome-desktop.nix
+++ modules/gnome-desktop.nix
@@ -1,5 +1,4 @@
-{ pkgs, ... }:
-{
+{pkgs, ...}: {
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
@@ -10,6 +9,20 @@
   # Exclude default GNOME bloat (Optional)
   # If you don't want Tour, Music, Epiphany, etc.
 
+  # 1. Create the custom "Clean" Thunderbird launcher
+  xdg.desktopEntries.thunderbird-clean = {
+    name = "Thunderbird (Clean Env)";
+    genericName = "Email Client";
+    exec = "env LD_LIBRARY_PATH= ${pkgs.thunderbird}/bin/thunderbird %u";
+    terminal = false;
+    categories = ["Application" "Network" "Email"];
+    mimeType = ["x-scheme-handler/mailto"];
+    icon = "thunderbird"; # Ensures the icon still shows up
+  };
+  # 2. remove the default Thunderbird launcher
+  # NOTE: This will remove the default Thunderbird launcher, but it will not remove the
+  # Thunderbird binary itself. You can still access it via the command line.
+  xdg.desktopEntries.thunderbird.enable = false;
 
   # Add GNOME specific system packages (like Tweaks)
   environment.systemPackages = with pkgs; [
