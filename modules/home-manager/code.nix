b3ab5f541c512cd93773780bdaafd9d516b52dfa
espdesign <evanpendergraft@gmail.com>
2026-03-28 20:25:40 -0400

    Add custom vscode extension.

--- modules/home-manager/code.nix
+++ modules/home-manager/code.nix
@@ -1,19 +1,45 @@
-{pkgs, hostname, ...}: {
+{ pkgs, hostname, ... }:
+{
   programs.vscode = {
     enable = true;
     # Use the standard vscode package or vscode-fhs for better compatibility with external binaries
     package = pkgs.vscode;
 
-    # 1. Customizing Extensions
-    # You can find these in the Nixpkgs registry
-    profiles.default.extensions = with pkgs.vscode-extensions; [
-      nonylene.dark-molokai-theme
-      jnoortheen.nix-ide
-      dbaeumer.vscode-eslint
-      esbenp.prettier-vscode
-      # For your web dev work (Payload CMS / Next.js)
-      bradlc.vscode-tailwindcss
-    ];
+    profiles.default.extensions =
+      with pkgs.vscode-extensions;
+      [
+        nonylene.dark-molokai-theme
+        jnoortheen.nix-ide
+        dbaeumer.vscode-eslint
+        esbenp.prettier-vscode
+
+        bradlc.vscode-tailwindcss
+      ]
+      ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+        # Adding extensions NOT in nixpkgs:
+        # 1. Find the extension on VS Code Marketplace (e.g., https://marketplace.visualstudio.com/items?itemName=PublisherName.extension-name)
+        # 2. Note the publisher name and extension name from the URL
+        # 3. Get the latest version number from the page
+        # 4. Run: nix store prefetch-file https://marketplace.visualstudio.com/_apis/public/gallery/publishers/<Publisher>/vsextensions/<extension>/<version>/vspackage
+        # 5. Add to extensionsFromVscodeMarketplace below with the sha256 hash
+        #
+        # Example:
+        # ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+        #   {
+        #     name = "extension-name";
+        #     publisher = "Publisher";
+        #     version = "1.0.0";
+        #     sha256 = "hash-from-step-4";
+        #   }
+        # ]
+        #
+        {
+          name = "beardedtheme";
+          publisher = "BeardedBear";
+          version = "10.1.0";
+          sha256 = "7MkvLEadzgB7af01lYibEOqHn9bvzlpgMTEiiQBlEzA=";
+        }
+      ];
 
     # 2. Customizing Editor Options (settings.json)
     profiles.default.userSettings = {
@@ -23,10 +49,13 @@
       "editor.fontSize" = if hostname == "sin-laptop" then 16 else 14;
 
       # Line Rulers (Good for maintaining code style/max line lengths)
-      "editor.rulers" = [80 120];
+      "editor.rulers" = [
+        80
+        120
+      ];
 
       # General UI Customization
-      "workbench.colorTheme" = "Dracula";
+      "workbench.colorTheme" = "Bearded Theme";
       "editor.formatOnSave" = true;
       "editor.minimap.enabled" = false;
 
@@ -36,4 +65,3 @@
     };
   };
 }
-
