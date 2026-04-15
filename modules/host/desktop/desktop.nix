0ddec8f8b800ed17c85f6958d2bf30535a8f1f6f
espdesign <evanpendergraft@gmail.com>
2026-04-15 19:28:45 -0400

    rename hosts to host

---
+++ modules/host/desktop/desktop.nix
@@ -0,0 +1,17 @@
+{ inputs, self, ... }:
+{
+  flake.nixosConfigurations.desktop = inputs.nixpkgs.lib.nixosSystem {
+    modules = with self.nixosModules; [
+      desktop
+    ];
+  };
+
+  flake.nixosModules.desktop =
+    { pkgs, ... }:
+    {
+      imports = with self.nixosModules; [
+        coreDefaults
+      ];
+
+    };
+}
